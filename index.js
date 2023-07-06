70f54a37cd7e5218c82004e516070d291b5adf27
Hoyong Jeong <71969575+ghdyd586@users.noreply.github.com>
2023-07-06 12:17:18 +0900

    Ver 4.0
    Add About page
    Add Merchandise Page
    Add Info page
    Add Playlist page
    Edit Map size
    Add Left Slide Menu

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document
+      .querySelector("#menuicon")
+      .addEventListener("click", function (e) {
+        if (document.querySelector(".sidebar").classList.contains("on")) {
+          //메뉴 slideOut
+          document.querySelector(".sidebar").classList.remove("on");
+        } else {
+          //메뉴 slideIn
+          document.querySelector(".sidebar").classList.add("on");
+          //slideIn시 menuBtn의 img src를 cross icon으로 변경
+        }
+      });
+  });
